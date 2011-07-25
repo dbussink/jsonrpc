79bfbca90d9ffc29f4fe4d7a3d5fd58e4f8a6408
Guido Serra <guido.serra@txtr.com>
2011-07-25 16:07:15 +0200

    As expressed here... http://en.wikipedia.org/wiki/JSON-RPC#Usage
    Either else the Net:HTTP library... will apply the
    "application/x-www-form-urlencoded" , by default
    http://www.ruby-doc.org/stdlib/libdoc/net/http/rdoc/classes/Net/HTTP.html#M001391

--- lib/jsonrpc.rb
+++ lib/jsonrpc.rb
@@ -16,8 +16,9 @@ module JsonRPC
     def request(method, params)
       result = {}
       params ||= {}
+      h = {"Content-Type", "application/json"}
       Net::HTTP.start(@address.host, @address.port) do |connection|
-        result = JSON.parse(connection.post(@address.path, {:method => method.to_s, :params => params}.to_json).body)
+        result = JSON.parse(connection.post(@address.path, {:method => method.to_s, :params => params}.to_json, h).body)
       end
       if error = result["error"]
         raise JsonRPCError, error["message"]
